21a9ff84d06996ebfb1902902a4cab0f733077bf
LucLry <lucleroy78@gmail.com>
2023-04-26 11:31:30 +0200

    p2c3-ex

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,26 @@
-import { plantList } from '../datas/plantList'
+import { plantList } from "../datas/plantList";
+import "../styles/ShoppingList.css";
 
 function ShoppingList() {
-	return <ul></ul>
+  const plantCategories = plantList.reduce((unique, item) => {
+    return unique.includes(item.category) ? unique : [...unique, item.category];
+  }, []);
+  return (
+    <>
+      <ul>
+        {plantCategories.map((cat, index) => (
+          <li className="cat" key={`${cat}-${index}`}>
+            {cat}
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {plantList.map((plant) => (
+          <li key={plant.id}>{plant.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-export default ShoppingList
+export default ShoppingList;
