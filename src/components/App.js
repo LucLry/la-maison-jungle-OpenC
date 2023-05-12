926fe9a69289143ba79b6731f16e9f016093c687
LucLry <lucleroy78@gmail.com>
2023-05-12 16:48:51 +0200

    add cart into local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,33 @@
-import { useState } from 'react'
-import Banner from './Banner'
-import logo from '../assets/logo.png'
-import Cart from './Cart'
-import Footer from './Footer'
-import ShoppingList from './ShoppingList'
-import '../styles/Layout.css'
+import { useState, useEffect } from "react";
+import Banner from "./Banner";
+import logo from "../assets/logo.png";
+import Cart from "./Cart";
+import Footer from "./Footer";
+import ShoppingList from "./ShoppingList";
+import "../styles/Layout.css";
 
 function App() {
-	const [cart, updateCart] = useState([])
-	return (
-		<div>
-			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>La maison jungle</h1>
-			</Banner>
-			<div className='lmj-layout-inner'>
-				<Cart cart={cart} updateCart={updateCart} />
-				<ShoppingList cart={cart} updateCart={updateCart} />
-			</div>
-			<Footer />
-		</div>
-	)
+  const savedCart = localStorage.getItem("cart");
+  const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : []);
+
+  // get cart from local storage on first render
+  useEffect(() => {
+    updateCart(JSON.parse(localStorage.getItem("cart")));
+  }, []);
+
+  return (
+    <div>
+      <Banner>
+        <img src={logo} alt="La maison jungle" className="lmj-logo" />
+        <h1 className="lmj-title">La maison jungle</h1>
+      </Banner>
+      <div className="lmj-layout-inner">
+        <Cart cart={cart} updateCart={updateCart} />
+        <ShoppingList cart={cart} updateCart={updateCart} />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
